76af2b81f0d209819e9aa7f16262623954a3930c
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-06-28 11:28:21 -0400

    Rewrote htmlRoutes require statement

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require("./routes/htmlRoutes");
+// Require HTML routes
+const htmlRoutes = require("./routes/htmlRoutes")
 
 // Initialize the app and create a port
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+//Routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
