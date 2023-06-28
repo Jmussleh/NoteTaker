eb664f286b9a8fbfe436fdb133406d3237952b26
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-06-28 11:34:51 -0400

    Changed file structure on routes

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 //app.use('/', htmlRoutes);
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // Start the server on the port
